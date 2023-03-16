a9c218cdbce5b89e57413744ef75338f42d7feda
ayoubfli <ayoubflieau@gmail.com>
2023-03-16 16:02:52 -0700

    task6 up

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - this is the main function
+ *
+ * Return 0 when successful
+ * */
 
 
 
@@ -7,6 +11,6 @@
 int main(void){
 
    printf("with proper grammar, but the outcome is a piece of art,");
-   return 0;
+   return (0);
 
 }

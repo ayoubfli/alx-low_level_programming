83589c126b13f654ea17d00a14e62f7479cc7ab2
ayoubfli <ayoubflieau@gmail.com>
2023-04-05 17:55:10 -0700

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,8 @@
-include "main.h"
+#include "main.h"
 
 /**
  * factorial - returns the factorial of a number
- * @n: number to return the factorial from
+ * @n: numbiier to return the factorial from
  *
  * Return: factorial of n
  */
